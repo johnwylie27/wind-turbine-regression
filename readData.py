43b75b9344d0810b891ef4ed86ed582d7fdb201f
johnwylie27 <wyliej@rpi.edu>
2023-04-02 19:54:58 -0400

    Added new features based on roughness and compiled one master list of all tests to consider.

--- readData.py
+++ readData.py
@@ -4,25 +4,59 @@
 
 import os
 from os.path import exists
+# import math
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+# def dfRmNaN(df):
+#     for i in range(len(df.columns)): # loop through the columns
+#         if 1:
+#             print()
+#     return df
+
 
 ## Check File Directory
-filedir = 'G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Data\\01.Wind-Tunnel\\Aerodynamic-Data\\corr'
+filedir = 'C:\\Users\\John Wylie\\Documents\\RPI\\Courses\\MANE 6962 Machine Learning\\Project Git\\wind-turbine-regression\\Aerodynamic-Data\\corr'
 files = os.listdir(filedir)
 
+## Load Test List from Excel
+fileLog = 'C:\\Users\\John Wylie\\Documents\\RPI\\Courses\\MANE 6962 Machine Learning\\Project Git\\wind-turbine-regression\\Aerodynamic-Data\\Runlog.csv'
+testlist = pd.read_csv(fileLog)
+testlist.fillna(0, inplace=True)
 ## Load Data from Files into List of Data Frames
 df_list = {}
+ind = 0
 for i in range(len(files)):
     flnm = filedir + '\\' + files[i]
     if exists(flnm):
         print('Success')
-        df_list[i] = pd.read_csv(flnm, sep='\t', header=0, skiprows = [1])
+        temp = pd.read_csv(flnm, sep='\t', header=0, skiprows = [1], index_col=False)
+        temp.fillna(0, inplace=True)
+        m = len(temp)
+        
+        ## Add new features to temporary dataframe
+        temp['Grade'] = testlist.iloc[i, 1]*np.ones(m)
+        temp['Grit Size'] = testlist.iloc[i, 2]*np.ones(m)
+        temp['Lower Coverage'] = testlist.iloc[i, 3]*np.ones(m)
+        temp['Upper Coverage'] = testlist.iloc[i, 4]*np.ones(m)
+        
+        ## Adjust indexing
+        if i != 0:
+            temp.index = temp.index + ind
+            ind = ind + len(temp)
+        else:
+            ind = len(temp)
+        df_list[i] = temp
+        
+        ## Add dataframe to master dataframe
+        if i == 0:
+            df = df_list[i]
+        else:
+            frames = [df, df_list[i]]
+            df = pd.concat(frames)
         print(flnm)
 
-## Load Test List from Excel
-testlist = pd.read_excel('G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Data\\01.Wind-Tunnel\\Aerodynamic-Data\\Runlog.xlsx')
 
 ## Add features from testlist to dataframes
 
