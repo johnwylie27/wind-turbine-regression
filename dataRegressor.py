99012ff3ea8482a2677a6fb0746248904ccb139e
johnwylie27 <wyliej@rpi.edu>
2023-04-10 21:09:17 -0400

    Normalized/centered data and created pairplot

--- dataRegressor.py
+++ dataRegressor.py
@@ -6,6 +6,11 @@ import pandas as pd
 import sklearn
 import seaborn as sns
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+
+## Starting Parameters
+plot = 0
 
 ## Load Data
 dataFileX = 'C:\\Users\\John Wylie\\Documents\\RPI\\Courses\\MANE 6962 Machine Learning\\Project Git\\wind-turbine-regression\\df_data_X.csv'
@@ -23,4 +28,25 @@ X2 = pd.DataFrame.to_numpy(dfX2) # make into numpy array
 # We don't need to repeat for y b/c it is the same for both full and abridged
 
 ## Preprocess Data
-# for i in range():
+# Get shapes of the data arrays
+m1, n1 = X1.shape
+m2, n2 = X2.shape
+o, p = y.shape
+
+# Make copies of the old non-normalized data
+X1_original = X1.copy() # have to make copy of np array to specify different address in memory
+X2_original = X2.copy() # otherwise all the changes to X2 will apply to X2_original!
+y_original = y.copy()
+
+# Normalize all values
+for i in range(n1):
+    X1[:,i] = (X1[:,i] - np.mean(X1[:,i])) / np.std(X1[:,i])
+for i in range(n2):
+    X2[:,i] = (X2[:,i] - np.mean(X2[:,i])) / np.std(X2[:,i])
+for i in range(p):
+    y[:,i] = (y[:,i] - np.mean(y[:,i])) / np.std(y[:,i])
+
+## Visualize the Data
+if plot == 1:
+    sns.pairplot(dfX2, kind='scatter')
+
