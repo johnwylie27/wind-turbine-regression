5fb4668833d14a93e1c23e76c74fd52ab5900ed0
johnwylie27 <wyliej@rpi.edu>
2023-04-11 22:41:19 -0400

    Added NN analysis to different subsets. Also generated and saved figures for those cases.

--- dataRegressor.py
+++ dataRegressor.py
@@ -16,7 +16,9 @@ from keras import optimizers
 
 ## Starting Parameters
 pp_plt = 0 # boolean to determine whether to plot pairplot figure
-nn = 1 # boolean to determine whether to run the neural network
+nn1 = 1 # boolean to determine whether to run the NN on the full data
+nn2 = 0 # boolean to determine whether to run the NN on the partial data
+nn3 = 0 # boolean to determine whether to run the NN on the pressure port data only
 FS = 15 # font size for plotting labels
 
 ## Load Data
@@ -34,15 +36,21 @@ dfX2 = pd.concat([dfX1[dfX1.columns[:5]], dfX1[dfX1.columns[-4:]]], axis=1)
 X2 = pd.DataFrame.to_numpy(dfX2) # make into numpy array
 # We don't need to repeat for y b/c it is the same for both full and abridged
 
+# Abridged Dataset with Pressure Ports Only
+dfX3 = dfX1[dfX1.columns[5:-4]]
+X3 = pd.DataFrame.to_numpy(dfX3) # make into numpy array
+
 ## Preprocess Data
 # Get shapes of the data arrays
 m1, n1 = X1.shape
 m2, n2 = X2.shape
+m3, n3 = X3.shape
 o, p = y.shape
 
 # Make copies of the old non-normalized data
 X1_original = X1.copy() # have to make copy of np array to specify different address in memory
 X2_original = X2.copy() # otherwise all the changes to X2 will apply to X2_original!
+X3_original = X3.copy()
 y_original = y.copy()
 
 # Normalize all values
@@ -50,6 +58,8 @@ for i in range(n1):
     X1[:,i] = (X1[:,i] - np.mean(X1[:,i])) / np.std(X1[:,i])
 for i in range(n2):
     X2[:,i] = (X2[:,i] - np.mean(X2[:,i])) / np.std(X2[:,i])
+for i in range(n3):
+    X3[:,i] = (X3[:,i] - np.mean(X3[:,i])) / np.std(X3[:,i])
 for i in range(p):
     y[:,i] = (y[:,i] - np.mean(y[:,i])) / np.std(y[:,i])
 
@@ -58,13 +68,50 @@ if pp_plt == 1:
     sns.pairplot(dfX2, kind='scatter')
 
 ## Split Data into Training and Testing Splits
-X1_train, X1_test, y_train, y_test = train_test_split(X1, y, test_size=0.3, random_state=123)
-X2_train, X2_test, y_train, y_test = train_test_split(X2, y, test_size=0.3, random_state=123)
+X1_tr, X1_test, y_tr, y_test = train_test_split(X1, y, test_size=0.3, random_state=123)
+X2_tr, X2_test, y_tr, y_test = train_test_split(X2, y, test_size=0.3, random_state=123)
+X3_tr, X3_test, y_tr, y_test = train_test_split(X3, y, test_size=0.3, random_state=123)
 
-if nn == 1:
-    alpha = 0.01 # learning rate for the optimizer
-    epo = 400 # number of epochs
+## Neural Networks
+alpha = 0.01 # learning rate for the optimizer
+epo = 200 # number of epochs
+
+if nn1 == 1: # Run NN on data with pressure ports
+    model = Sequential()
+    model.add(Dense(100, input_dim=n1, activation='relu')) # one input neuron
+    model.add(Dense(100, activation='relu')) # two neurons in single hidden layer
+    model.add(Dense(3)) # one output neuron
+    opt = tf.keras.optimizers.experimental.SGD(learning_rate=alpha, momentum=0) # stochastic gradient descent optimizer
+    mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM) # mean squared error for loss calculation
+    model.compile(loss=mse, optimizer=opt, metrics=['mse', 'mae', 'mape'])
+    tr_history1 = model.fit(X1_tr, y_tr, epochs=epo, validation_data=(X1_test, y_test), verbose=0, use_multiprocessing=-3) # validation for monitoring validation loss and metrics at the end of each epoch
+    model.summary()
+    y_pred1 = model.predict(X1_test, verbose=0, use_multiprocessing=-3)
+    test_loss1 = mean_squared_error(y_test, y_pred1)
+    print(test_loss1)
     
+    # Plot Results
+    plt.figure(figsize=(8, 5))
+    plt.plot(tr_history1.epoch, tr_history1.history['loss'], 'o-', linewidth=0.5, markersize=2, label=('training loss'))
+    plt.plot(tr_history1.epoch, tr_history1.history['val_loss'], 'o-', linewidth=0.5, markersize=2, label=('validation loss'))
+    plt.xlabel('number of epochs', fontsize = FS)
+    plt.ylabel('loss', fontsize = FS)
+    plt.ylim([0, 1.2*np.max(tr_history1.history['val_loss'])])
+    plt.title('Training/Testing Loss Comparison: Full Data', fontsize = FS)
+    plt.legend(loc='upper right')
+    plt.savefig('G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Figures\\X1_loss.png', dpi=300)
+    
+    plt.figure(figsize=(8, 5))
+    plt.plot(tr_history1.epoch, tr_history1.history['mae'], 'o-', linewidth=0.5, markersize=2, label=('training mse'))
+    plt.plot(tr_history1.epoch, tr_history1.history['val_mae'], 'o-', linewidth=0.5, markersize=2, label=('validation mse'))
+    plt.xlabel('number of epochs', fontsize = FS)
+    plt.ylabel('loss metric: mae', fontsize = FS)
+    plt.ylim([0, 1.2*np.max(tr_history1.history['val_mae'])])
+    plt.title('Training/Testing Loss Comparison: Full Data', fontsize = FS)
+    plt.legend(loc='upper right')
+    plt.savefig('G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Figures\\X1_mae.png', dpi=300)
+    
+if nn2 == 1: # Run NN on data without pressure ports
     model = Sequential()
     model.add(Dense(10, input_dim=n2, activation='relu')) # one input neuron
     model.add(Dense(10, activation='relu')) # two neurons in single hidden layer
@@ -72,7 +119,7 @@ if nn == 1:
     opt = tf.keras.optimizers.experimental.SGD(learning_rate=alpha, momentum=0) # stochastic gradient descent optimizer
     mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM) # mean squared error for loss calculation
     model.compile(loss=mse, optimizer=opt, metrics=['mse', 'mae', 'mape'])
-    tr_history2 = model.fit(X2_train, y_train, epochs=epo, validation_data=(X2_test, y_test), verbose=0, use_multiprocessing=-3) # validation for monitoring validation loss and metrics at the end of each epoch
+    tr_history2 = model.fit(X2_tr, y_tr, epochs=epo, validation_data=(X2_test, y_test), verbose=0, use_multiprocessing=-3) # validation for monitoring validation loss and metrics at the end of each epoch
     model.summary()
     y_pred2 = model.predict(X2_test, verbose=0, use_multiprocessing=-3)
     test_loss2 = mean_squared_error(y_test, y_pred2)
@@ -84,13 +131,52 @@ if nn == 1:
     plt.plot(tr_history2.epoch, tr_history2.history['val_loss'], 'o-', linewidth=0.5, markersize=2, label=('validation loss'))
     plt.xlabel('number of epochs', fontsize = FS)
     plt.ylabel('loss', fontsize = FS)
-    plt.title('Training/Testing Loss Comparison', fontsize = FS)
+    plt.ylim([0, 1.2*np.max(tr_history2.history['val_loss'])])
+    plt.title('Training/Testing Loss Comparison: Partial Data', fontsize = FS)
     plt.legend(loc='upper right')
+    plt.savefig('G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Figures\\X2_loss.png', dpi=300)
     
     plt.figure(figsize=(8, 5))
     plt.plot(tr_history2.epoch, tr_history2.history['mae'], 'o-', linewidth=0.5, markersize=2, label=('training mse'))
     plt.plot(tr_history2.epoch, tr_history2.history['val_mae'], 'o-', linewidth=0.5, markersize=2, label=('validation mse'))
     plt.xlabel('number of epochs', fontsize = FS)
     plt.ylabel('loss metric: mae', fontsize = FS)
-    plt.title('Training/Testing Loss Comparison', fontsize = FS)
+    plt.ylim([0, 1.2*np.max(tr_history2.history['val_mae'])])
+    plt.title('Training/Testing Loss Comparison: Partial Data', fontsize = FS)
     plt.legend(loc='upper right')
+    plt.savefig('G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Figures\\X2_mae.png', dpi=300)
+    
+if nn3 == 1: # Run NN on pressure port data only
+    model = Sequential()
+    model.add(Dense(10, input_dim=n3, activation='relu')) # one input neuron
+    model.add(Dense(10, activation='relu')) # two neurons in single hidden layer
+    model.add(Dense(3)) # one output neuron
+    opt = tf.keras.optimizers.experimental.SGD(learning_rate=alpha, momentum=0) # stochastic gradient descent optimizer
+    mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM) # mean squared error for loss calculation
+    model.compile(loss=mse, optimizer=opt, metrics=['mse', 'mae', 'mape'])
+    tr_history3 = model.fit(X3_tr, y_tr, epochs=epo, validation_data=(X3_test, y_test), verbose=0, use_multiprocessing=-3) # validation for monitoring validation loss and metrics at the end of each epoch
+    model.summary()
+    y_pred3 = model.predict(X3_test, verbose=0, use_multiprocessing=-3)
+    test_loss3 = mean_squared_error(y_test, y_pred3)
+    print(test_loss3)
+    
+    # Plot Results
+    plt.figure(figsize=(8, 5))
+    plt.plot(tr_history3.epoch, tr_history3.history['loss'], 'o-', linewidth=0.5, markersize=2, label=('training loss'))
+    plt.plot(tr_history3.epoch, tr_history3.history['val_loss'], 'o-', linewidth=0.5, markersize=2, label=('validation loss'))
+    plt.xlabel('number of epochs', fontsize = FS)
+    plt.ylabel('loss', fontsize = FS)
+    plt.ylim([0, 1.2*np.max(tr_history3.history['val_loss'])])
+    plt.title('Training/Testing Loss Comparison: Partial Data', fontsize = FS)
+    plt.legend(loc='upper right')
+    plt.savefig('G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Figures\\X3_loss.png', dpi=300)
+    
+    plt.figure(figsize=(8, 5))
+    plt.plot(tr_history3.epoch, tr_history3.history['mae'], 'o-', linewidth=0.5, markersize=2, label=('training mse'))
+    plt.plot(tr_history3.epoch, tr_history3.history['val_mae'], 'o-', linewidth=0.5, markersize=2, label=('validation mse'))
+    plt.xlabel('number of epochs', fontsize = FS)
+    plt.ylabel('loss metric: mae', fontsize = FS)
+    plt.ylim([0, 1.2*np.max(tr_history3.history['val_mae'])])
+    plt.title('Training/Testing Loss Comparison: Partial Data', fontsize = FS)
+    plt.legend(loc='upper right')
+    plt.savefig('G:\\My Drive\\RPI\\MANE 6962 Machine Learning\\Project\\Figures\\X3_mae.png', dpi=300)
